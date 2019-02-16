881c97a45706a32bc4e469cd992b735eceadfe6d
Dimitri Roche <dimroc@gmail.com>
2019-02-16 15:39:52 -0700

    index shows jokes,listens to updates:
    can add joke w
    curl localhost:3000/testing_firebase

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,35 @@
-import seed from '../components/seed'
+import React, { useContext, useEffect, useState } from 'react'
+import { seed } from '../components/seed'
+import { FirebaseContext } from '../components/FirebaseContext'
+import 'firebase/firestore'
 
 const Home = () => {
-  return <div>Welcome to next.js!</div>
+  const firebase = useContext(FirebaseContext)
+  const [jokes, setJokes] = useState([])
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection('dimitris_jokes')
+      .onSnapshot(querySnapshot => {
+        const queryJokes = []
+        querySnapshot.forEach(doc => {
+          queryJokes.push(doc.data())
+        })
+        setJokes(queryJokes)
+      })
+  })
+
+  return (
+    <React.Fragment>
+      <div>Welcome to next.js!</div>
+      <ul>
+        {jokes.map(j => (
+          <li>{j.message}</li>
+        ))}
+      </ul>
+    </React.Fragment>
+  )
 }
 
 export default seed(Home)

14995a971e41b9507f79d2b05e66917528ba2f3e
John Barker <jebarker@gmail.com>
2019-02-15 21:37:20 -0700

    Add webhook endpoint which just console.logs

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // This file doesn't go through babel or webpack transformation.
 // Make sure the syntax and sources this file requires are compatible with the current node version you are running
 // See https://github.com/zeit/next.js/issues/1245 for discussions on Universal Webpack or universal Babel
+const express = require('express')
 const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
 const fb = require('./server/firebase')
+const bodyParser = require('body-parser')
 
 const dev = process.env.NODE_ENV !== 'production'
 const port = process.env.PORT || 3000
@@ -12,9 +14,15 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
+  const server = express()
+
+  server.use(bodyParser.json())
+
+  server.post('/gh_webhooks', (req, res) => {
+    console.log('got webook', req.body)
+  })
+
+  server.get('*', (req, res) => {
     const parsedUrl = parse(req.url, true)
     handle(req, res, parsedUrl)
   }).listen(port, err => {

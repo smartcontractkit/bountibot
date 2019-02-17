c30ff5e6fed1711245524e73f5994db6bc53d2d9
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-02-16 23:28:18 -0700

    Add robot pirate to home

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,35 @@
 import React, { useContext, useEffect, useState } from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
 import fetch from 'isomorphic-unfetch'
 import { initialPropsBaseUrl as baseUrl, seed } from '../components/seed'
 import { FirebaseContext } from '../components/FirebaseContext'
 import 'firebase/firestore'
+import robotPirate from '../images/robot-pirate.png'
 
-const Home = ({ seedJokes }) => {
+const styles = _theme => {
+  return {
+    container: {
+      marginTop: 40
+    },
+    pirateContainer: {
+      position: 'relative'
+    },
+    greeting: {
+      backgroundColor: '#fff',
+      border: 'solid 1px #f3f3f3',
+      borderRadius: 20,
+      position: 'absolute',
+      top: 50,
+      marginLeft: 300,
+      padding: 30,
+      fontSize: 19
+    }
+  }
+}
+
+const Home = ({ seedJokes, classes }) => {
   const firebase = useContext(FirebaseContext)
   const [jokes, setJokes] = useState(seedJokes)
 
@@ -22,13 +47,21 @@ const Home = ({ seedJokes }) => {
   })
 
   return (
-    <>
-      <ul>
-        {jokes.map((j, idx) => (
-          <li key={idx}>{j.message}</li>
-        ))}
-      </ul>
-    </>
+    <Grid container justify="center" className={classes.container}>
+      <Grid item xs={3}></Grid>
+      <Grid item xs={6}>
+        <div className={classes.pirateContainer}>
+          <img
+            src={robotPirate}
+            width={400}
+          />
+          <Typography variant="body1" component='div' className={classes.greeting}>
+            Start contributin' Pull Requests t' earn yourself a tidy Bounty o' LINK
+          </Typography>
+        </div>
+      </Grid>
+      <Grid item xs={3}></Grid>
+    </Grid>
   )
 }
 
@@ -39,4 +72,4 @@ Home.getInitialProps = async ({ req }) => {
   return { seedJokes: resJSON.jokes }
 }
 
-export default seed(Home)
+export default withStyles(styles)(seed(Home))
